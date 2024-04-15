f7ebb232b8ea020a13cdae75eeac593178bdc8cb
Arthur RICHARD <arthur.richard@protonmail.com>
2024-04-15 22:57:12 +0200

    :recycle: reworking the SGConnectAuth

--- setup.py
+++ setup.py
@@ -1,15 +1,46 @@
-# -*- coding: UTF-8 -*-
+from pip import download, req
+from setuptools import find_packages, setup
 
-from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+links = []
+requires = []
+
+try:
+    requirements = req.parse_requirements("requirements.txt")
+except:
+    requirements = req.parse_requirements(
+        "requirements.txt", session=download.PipSession()
+    )
+
+for item in requirements:
+    if getattr(item, "url", None):  # older pip has url
+        links.append(str(item.url))
+    if getattr(item, "link", None):  # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req))
 
 setup(
-    name='pysgconnect',
+    name="pysgconnect",
     use_scm_version=True,
+    url="https://github.com/societe-generale/pysgconnect",
     packages=find_packages(),
-    install_requires=['requests<=2.9.1'],
+    install_requires=requires,
+    dependency_links=links,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     setup_requires=[
-        'setuptools_scm',
-        'wheel',
+        "setuptools_scm",
+        "wheel",
     ],
-    description='Utilities to interact with SGConnect'
+    description="Utilities to interact with SGConnect",
+    python_requires=">=3.10",
+    platforms="any",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
