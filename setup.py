140e6687e4fda2ad7ccd1a551d360b27a27933e0
Arthur RICHARD <arthur.richard@protonmail.com>
2024-05-06 12:46:03 +0200

    testrelease2

--- setup.py
+++ setup.py
@@ -1,35 +1,17 @@
-from pip import download, req
 from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-links = []
-requires = []
-
-try:
-    requirements = req.parse_requirements("requirements.txt")
-except:
-    requirements = req.parse_requirements(
-        "requirements.txt", session=download.PipSession()
-    )
-
-for item in requirements:
-    if getattr(item, "url", None):  # older pip has url
-        links.append(str(item.url))
-    if getattr(item, "link", None):  # newer pip has link
-        links.append(str(item.link))
-    if item.req:
-        requires.append(str(item.req))
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="pysgconnect",
     version="1.0",
     url="https://github.com/societe-generale/pysgconnect",
-    package_dir={'':"pysgconnect"},
     packages=find_packages(),
-    install_requires=requires,
-    dependency_links=links,
+    install_requires=requirements,
     long_description=long_description,
     long_description_content_type="text/markdown",
     description="Utilities to interact with SGConnect",
